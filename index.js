dd738a8d14db71ba0e0bff582e1057d32b236a7b
Joshua Comeau <joshwcomeau@gmail.com>
2017-08-05 09:01:23 -0400

    Use commander for basic param parsing

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-console.log('Hello world!')
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+const program = require('commander');
+
+// Load our package.json, so that we can pass the version onto `commander`.
+const {version} = require('./package.json');
+
+const rootPath = path.resolve();
+
+program
+  .version(version)
+  .arguments('<filename>')
+  .option(
+    '-t, --type <componentType>',
+    'Type of React component to generate',
+    /^(class|pureClass|functional)$/i,
+    'class'
+  ).option(
+    '-s, --style <style>',
+    'Type of styles to use',
+    /^(css-modules|aphrodite|styled-components|emotion)$/i
+  );
