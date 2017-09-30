11e18c18cb7cc49268c0b02b998b99cf77735dee
Joshua Comeau <joshwcomeau@gmail.com>
2017-09-30 14:19:35 -0400

    Add Prettier, use Husky and lint-staged to auto-commit prettification

--- src/templates/functional.js
+++ src/templates/functional.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const COMPONENT_NAME = () => {
   return <div />;
 };
--- src/utils.js
+++ src/utils.js
@@ -12,8 +12,7 @@ For application-specific concerns, use `helpers.js`.
 const fs = require('fs');
 const path = require('path');
 
-
-module.exports.requireOptional = (filePath) => {
+module.exports.requireOptional = filePath => {
   try {
     return require(filePath);
   } catch (e) {
@@ -26,36 +25,32 @@ module.exports.requireOptional = (filePath) => {
   }
 };
 
-module.exports.mkDirPromise = (dirPath) => (
+module.exports.mkDirPromise = dirPath =>
   new Promise((resolve, reject) => {
-    fs.mkdir(dirPath, (err) => {
+    fs.mkdir(dirPath, err => {
       err ? reject(err) : resolve();
     });
-  })
-);
+  });
 
 // Simple promise wrappers for read/write files.
 // utf-8 is assumed.
-module.exports.readFilePromise = fileLocation => (
+module.exports.readFilePromise = fileLocation =>
   new Promise((resolve, reject) => {
     fs.readFile(fileLocation, 'utf-8', (err, text) => {
       err ? reject(err) : resolve(text);
     });
-  })
-);
+  });
 
-module.exports.writeFilePromise = (fileLocation, fileContent) => (
+module.exports.writeFilePromise = (fileLocation, fileContent) =>
   new Promise((resolve, reject) => {
-    fs.writeFile(fileLocation, fileContent, 'utf-8', (err) => {
+    fs.writeFile(fileLocation, fileContent, 'utf-8', err => {
       err ? reject(err) : resolve();
     });
-  })
-);
+  });
 
 // Somewhat counter-intuitively, `fs.readFile` works relative to the current
 // working directory (if the user is in their own project, it's relative to
 // their project). This is unlike `require()` calls, which are always relative
 // to the code's directory.
-module.exports.readFilePromiseRelative = fileLocation => (
-  module.exports.readFilePromise(path.join(__dirname, fileLocation))
-);
+module.exports.readFilePromiseRelative = fileLocation =>
+  module.exports.readFilePromise(path.join(__dirname, fileLocation));
