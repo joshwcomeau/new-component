7be9817dce25b6248f558d5845cad50b50ae6bcc
LB <mail@lb.ee>
2018-06-16 15:17:41 +1000

    update spec file to use execa & test component creation

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,52 @@
-var spawnCommand = require('spawn-command');
+const path = require('path');
+const execa = require('execa');
+const CLI_PATH = path.resolve(__dirname, '../src/index.js');
 
-const CLI_PATH = require.resolve('../src/index');
+const clearComponentsDirectory = () => execa.sync('rm', ['-rf', './src/components']);
 
-describe('new-component CLI', () => {
-  it('should accept --help argument', done => {
-    child = spawnCommand(CLI_PATH + ' --help');
-
-    child.stdout.on('data', function (data) {
-      const output = data.toString();
-      expect(output).toMatchSnapshot();
-      expect(output).toContain('-t, --type <componentType>');
+describe('echo CLI test', () => {
+  it('should be able to read stdout', done => {
+    execa('echo', ['unicorns']).then(result => {
+      expect(result.stdout).toEqual('unicorns');
+      done();
     });
+  });
+});
 
-    child.on('exit', function (exitCode) {
-      expect(exitCode).toEqual(0);
-      done(); // must always call on exit to test async code
+describe('new-component - help', () => {
+  it('should accept --help argument ASYNC', done => {
+    // example test using async execa
+    execa(CLI_PATH, ['--help']).then(result => {
+      expect(result.stdout).toMatchSnapshot();
+      expect(result.stdout).toContain('-t, --type <componentType>');
+      done();
     });
+  });
+  it('should accept --help argument SYNC', () => {
+    // example test using sync execa
+    const { code, stdout } = execa.sync(CLI_PATH, ['--help']);
+    expect(code).toBe(0);
+    expect(stdout).toMatchSnapshot();
   })
 });
+
+describe('new-component - creating a new component', () => {
+  beforeEach(clearComponentsDirectory);
+  afterEach(clearComponentsDirectory);
+  it('should show console error if no components folder', () => {
+    const { code, stdout } = execa.sync(CLI_PATH, ['CoolButton']);
+    expect(code).toBe(0);
+    expect(stdout).toContain('Sorry, you need to create a parent \"components\" directory.');
+    expect(stdout).toMatchSnapshot();
+  });
+  it('should create a new component file for CoolButton', () => {
+    execa.sync('mkdir', ['./src/components']);
+    const { code, stdout } = execa.sync(CLI_PATH, ['CoolButton']);
+    const expectedPath = 'src/components/CoolButton/CoolButton.js';
+    const pathToNewComponent = path.resolve('./' + expectedPath);
+    expect(pathToNewComponent).toContain(expectedPath);
+    expect(code).toBe(0);
+    expect(stdout).toMatchSnapshot();
+    expect(stdout).toContain('Creating the CoolButton component');
+  });
+})
